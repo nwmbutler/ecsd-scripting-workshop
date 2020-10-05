3ce758024649c261867d5bc6bfd8d9760c74e6e7
Nick Butler <nwmbutler@gmail.com>
2020-10-05 14:24:48 +0100

    if statement updated with outer brackets

--- task_2/task_2.sh
+++ task_2/task_2.sh
@@ -3,11 +3,11 @@ Mod=$(( 73 % 8 ))
 echo $Mod
 
 //2
-PowOf=$(( 4*6 + 5*3 ))
+PowOf=$(( 4**6 + 5**3 ))
 echo $PowOf
 
 //3
-echo $(( 4*6 + 5*3 ))
+echo $(( 4**6 + 5**3 ))
 
 //4
-if [ 2 = 2 ]; then echo 'Equal'; else echo 'Not equal'; fi
+if [[ $int1 == $int2 ]]; then; echo 'Equal'; else echo 'Not equal'; fi

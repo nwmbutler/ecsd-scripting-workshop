49241e608f8ad2937ea05bc9d778b8cea9e9e3d9
Nick Butler <nwmbutler@gmail.com>
2020-10-05 14:57:36 +0100

    task 3 part 1 complete

---
+++ task_3/task_3.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+// 1
+
+if [ $int3 == 4 ]; then
+    echo "The number is 4"
+    exit 0
+fi
+
+if [ $int3 == 5 ]; then
+    echo "The number is 5"
+    exit 1
+fi 
+
+if [ $int3 != 5 ]; then
+    echo "The number is not 5"
+    exit 2
+fi
+
+// 2
+

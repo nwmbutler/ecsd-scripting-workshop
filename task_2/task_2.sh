609cbaa5bd67151c92b6f40579a8d54d3cdecdc3
Nick Butler <nwmbutler@gmail.com>
2020-10-05 14:11:22 +0100

    Task 2 attempt completed

---
+++ task_2/task_2.sh
@@ -0,0 +1,13 @@
+// 1
+Mod=$(( 73 % 8 ))
+echo $Mod
+
+//2
+PowOf=$(( 4*6 + 5*3 ))
+echo $PowOf
+
+//3
+echo PowOf=$(( 4*6 + 5*3 ))
+
+//4
+if [ 2 = 2 ]; then echo 'Equal'; else echo 'Not equal'; fi

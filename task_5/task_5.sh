b238d80d0735559afdcb53e4b1331b3fe9a10001
Nick Butler <nwmbutler@gmail.com>
2020-10-07 11:58:11 +0100

    Task 5 attempt

--- task_5/task_5.sh
+++ task_5/task_5.sh
@@ -1,12 +1,20 @@
 // 1
 
-PASSWORD="12345"
+PASSWORD=" "
+COUNT=0
+
 echo "Please enter your password: "
 read input
-while [ $input != $PASSWORD ]; do
-echo "Please try again"
+$PASSWORD=$input
+while [[ $PASSWORD != "12345" && $COUNT -lt 10 ]]; do
+  echo "Incorrect please try again: "
+  read input
+  $PASSWORD=$input
+  COUNT=$((COUNT+1))
 done
 
 // 2
 
-
+for i in {1..10};  do
+  echo "$(($i * $i))"
+done 

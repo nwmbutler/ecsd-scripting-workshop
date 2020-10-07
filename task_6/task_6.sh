9e32d19c64f8b5fe95ed85181703268c977292e7
Nick Butler <nwmbutler@gmail.com>
2020-10-07 14:33:27 +0100

    task_6 completed

--- task_6/task_6.sh
+++ task_6/task_6.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-myArray=(Hello my name is Nick and I am learning scripting)
-for Item in $myArray ;
-  do
-    echo $Item
-  done
+strArray=(Hello my name is Nick and I am getting to grips with scripting)
+
+echo "which character would you like to replace: "
+read charOut
+echo "what character would you like to substitute: "
+read charIn
+
+strArray=("${strArray[@]/$charOut/$charIn}")
+
+for char in ${strArray[@]}
+do
+    echo $char
+done
